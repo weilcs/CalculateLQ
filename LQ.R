71f86d5c1f82a2bfb01c7ac554fcc28bf6d6e960
weilcs <45430597+weilcs@users.noreply.github.com>
2019-08-03 21:29:16 -0230

    Add function to calculate LQ change over time

--- LQ.R
+++ LQ.R
@@ -1,20 +1,8 @@
 library(data.table)
 library(dplyr)
+library(xlsx)
+rm(list = ls())
 df <- data.table(read.csv("BREmpData_0107.csv", header = TRUE))
-#GeogIDs <- unique(df[, 1])
-#IndIDs <- unique(df[, 2])
-#Years <- unique(df[, 6])
-#Employs <- df[, 7]
-
-
-#setkey(df, GeogID, IndID, YEAR)
-
-
-
-#df_sub = df[.(3, 3, 2001)]
-#aa = sum(df_sub$Employment)
-
-  
 
 df_reg_ind <- df[, sum(Employment), by = list(GeogID, IndID, YEAR)]
 df_reg_total <- df[, sum(Employment), by = list(GeogID, YEAR)]
@@ -30,3 +18,22 @@ df2 <- full_join(x = df_prov_ind, y = df_prov_total)
 df2$prov_ration <- (df2$E_i/df2$E_t)
 df3 <- full_join(df1, df2)
 df3$LQ <- (df3$reg_ratio/df3$prov_ration)
+write.table(df3, "LQ.csv",  row.names = F, sep = ",")
+
+lq_2001 <- subset(df3, df3[,3] == 2001)
+lq_2007 <- subset(df3, df3[,3] == 2007)
+
+x <- lq_2001[, c(1,2,10)]
+y <- lq_2007[, c(1,2,10)]
+setnames(x, 'LQ', 'LQ_2001')
+setnames(y, 'LQ', 'LQ_2007')
+lq_changed <- inner_join(x,y)
+lq_changed$ratio <- (lq_changed$LQ_2007 - lq_changed$LQ_2001)/lq_changed$LQ_2001
+
+
+geo_code <- distinct(df[, c(1, 4)])
+lq_changed <- inner_join(lq_changed, geo_code)
+
+ind_code <- distinct(df[, c(2, 5)])
+lq_changed <- inner_join(lq_changed, ind_code)
+write.table(lq_changed, "LQ_Changed.csv",  row.names = F, sep = ",")
